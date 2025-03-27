9bf4fd985bb07daf0dd568f42baf9cd1844898d7
kakoikeisuke <k.kakoi0531@gmail.com>
2025-03-27 20:00:42 +0900

    プロジェクトフォルダ情報の取得に関する処理を追加

--- QuickOpenProjectFolder/plug-ins/QuickOpenProjectFolder.py
+++ QuickOpenProjectFolder/plug-ins/QuickOpenProjectFolder.py
@@ -1,3 +1,4 @@
+import os
 import maya.cmds as cmds
 import maya.mel as mel
 import maya.api.OpenMaya as om
@@ -50,21 +51,51 @@ def uninitializePlugin(plugin):
         cmds.error('メニューの削除に失敗しました。')
 
 def create_ui():
+    global job
+    job = cmds.scriptJob(event=['workspaceChanged', update_ui()])
+
+    project_info = get_project_info()
+
     main_window = mel.eval('$gmw = $gMainWindow')
     if cmds.menu('ProjectFolder', exists=True):
         cmds.deleteUI('ProjectFolder')
-    custom_menu = cmds.menu('ProjectFolder', parent=main_window, label='ProjectFolder', tearOff=True)
-    cmds.menuItem(
-        label='ここにフォルダ名',
-        parent=custom_menu,
-        command='ここにフォルダーを開くコード',
-        image='アイコン',
-        annotation='フォルダー名'
-    )
+
+    project_name = os.path.basename(os.path.normpath(project_info[0]))
+
+    custom_menu = cmds.menu('ProjectFolder', parent=main_window, label=project_name, tearOff=True)
+
+    for i in range(len(project_info)-1):
+        folder_name = os.path.basename(os.path.normpath(project_info[i+1]))
+        cmds.menuItem(
+            label=folder_name,
+            parent=custom_menu,
+            command='実行コマンド',
+            image='アイコンパス',
+            annotation=project_info[i+1] + ' を開きます。'
+        )
 
 def delete_ui():
+    if cmds.scriptJob(exists=job):
+        cmds.scriptJob(kill=job)
     if cmds.menu('ProjectFolder', exists=True):
         cmds.deleteUI('ProjectFolder')
 
+def update_ui():
+    delete_ui()
+    create_ui()
+
 def test():
     print('これでどう？')
+
+def get_project_info():
+    project_info = []
+    project_path = cmds.workspace(query=True, rootDirectory=True)
+    # project_name = os.path.basename(
+    #     os.path.normpath(project_path)
+    # )
+    project_info.append(project_path)
+    all_item = os.listdir(project_path)
+    for i in range(len(all_item)):
+        if os.path.isdir(project_path + all_item[i]):
+            project_info.append(all_item[i])
+    return project_info
